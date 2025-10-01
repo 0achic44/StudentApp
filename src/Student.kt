c877e20819db960ef17be8e435cbccf547c052b0
Maria Achim <0achic44@solent.ac.uk>
2025-10-01 14:47:29 +0100

    Added grades and mark validation

--- src/Student.kt
+++ src/Student.kt
@@ -1,8 +1,13 @@
-class Student (val nId: String, val nName: String, val nCourse: String, val nMark: Double) {
+class Student (val nId: String, val nName: String, val nCourse: String, nMark: Double) {
     val id: String
     val name: String
     val course: String
-    val mark: Double
+    var mark = 0.0
+        set (newMark) {
+            if (newMark <= 100 && newMark >= 0) {
+                field = newMark
+            }
+        }
 
     init {
         id = nId
@@ -11,7 +16,18 @@ class Student (val nId: String, val nName: String, val nCourse: String, val nMar
         mark = nMark
     }
 
+    fun getGrade(): String {
+        when (mark) {
+            in 70.0 .. 100.0 -> return "First"
+            in 60.0 .. 69.0 -> return "2/1"
+            in 50.0 .. 59.0 -> return "2/2"
+            in 40.0 .. 49.0 -> return "Third"
+            else -> return "Fail"
+        }
+    }
+
     override fun toString(): String {
-        return "Id: $nId, Name: $nName, Course: $nCourse, Mark: $mark"
+        val grade = getGrade()
+        return "Id: $nId, Name: $nName, Course: $nCourse, Mark: $mark, Grade: $grade"
     }
 }
